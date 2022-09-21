06135ca52e2dff6d9870654e0979a6445a3d6e2c
Andrew Chang <andrewchang@Andrews-MacBook-Pro.local>
2022-09-20 17:07:21 -0700

    Knowledge Test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,38 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    receive() payable external {
+        // Do nothing
+    }
+
+    function changeTokens() public {
+        tokens[0] = "VET";
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable destination) public onlyOwner {
+        destination.transfer(address(this).balance);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string memory a, string memory b) public pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
     }
 }
